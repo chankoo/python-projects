2dca5bdc1c17315fb112944a4ff4e22d68e22f95
chankoo <ckbaek1125@gmail.com>
2019-03-09 13:34:28 +0900

    텍스트기반 전시예술 검색 시스템 18.08.29

--- Arts Patronage/Art웹/ExhbRec/input.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def art_input():
-    western_data = pd.read_csv('static/data/western_preprocessed.csv')
-    famous_paint = western_data[['title', 'artist', 'image']].sample(25)
-    return famous_paint
